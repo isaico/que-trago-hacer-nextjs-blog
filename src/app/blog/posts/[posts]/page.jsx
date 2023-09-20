3751f79ffaf9b4f3fd0d9743385a4952ce28da2d
isaias garcia <isaico@outlook.com>
2023-09-20 01:34:01 -0300

    route errors, try to fix by removing the dynamic bloglayout

--- src/app/blog/posts/[posts]/page.jsx
+++ src/app/blog/posts/[posts]/page.jsx
@@ -1,14 +1,12 @@
 import fetchBlog from '@/utils/fetchBlog';
-// import BlogLayout from '@/components/layoutComps/Blog/BlogLayout';
+import BlogLayout from '@/components/layoutComps/Blog/BlogLayout';
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 const ErrorFetchData = dynamic(() =>
     import('@/components/UiComps/ErrorFetchData')
 );
 const Loader = dynamic(() => import('@/components/UiComps/Loader'));
-const BlogLayout = dynamic(() =>
-    import('@/components/layoutComps/Blog/BlogLayout')
-);
+
 
 const Posts = async ({ params }) => {
     const post = params.posts.replace(/-/g, ' ');
